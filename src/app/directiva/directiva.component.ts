12c2ab1042d99d012874b3c35024b0c02b935259
Gustavo Espinosa <age.espinosa@gmail.com>
2019-06-17 14:15:23 -0300

    Directiva estructural Ng-For

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+  listaCurso: string[] = ['typeScript', 'JavaScript', 'JAva SE', 'C#', 'PHP'];
+  constructor() { }
+
+
+
+}
