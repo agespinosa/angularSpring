d3f43818d53e5ab6bdc919efd6b9b779bfb75332
Gustavo Espinosa <age.espinosa@gmail.com>
2019-06-17 15:11:47 -0300

    Listado de clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+
+  public clientes: Cliente[] =
+  [
+    {id: 1, nombre: 'Gustavo', apellido: 'Espinosa', createdAt: '2019-06-17', email: 'gustavo@gmail.com'},
+    {id: 2, nombre: 'Juan', apellido: 'Agostin', createdAt: '2019-06-17', email: 'juan@gmail.com'},
+    {id: 3, nombre: 'Victor', apellido: 'Nardoni', createdAt: '2019-06-17', email: 'voctrr@gmail.com'},
+    {id: 4, nombre: 'Rene', apellido: 'Recalde', createdAt: '2019-06-17', email: 'rene@gmail.com'},
+    {id: 5, nombre: 'Andres', apellido: 'Brusutti', createdAt: '2019-06-17', email: 'andres@gmail.com'},
+  ];
 
   constructor() { }
 
