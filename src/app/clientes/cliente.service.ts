6ee6de5b8154761ac279993626b3d0229f1d43cb
gustavo espinosa <age.espinosa@gmail.com>
2019-07-15 12:30:42 -0300

    implementacion crear cliente service

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ClienteService {
 
   private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+  private httpHeader= new HttpHeaders({'Content-Type': 'application/json'});
+
   constructor(private http: HttpClient) { }
 
   public getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
+
+  create(cliente:Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeader});
   }
 }
